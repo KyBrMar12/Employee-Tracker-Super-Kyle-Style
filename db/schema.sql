257340c63b09e65c1b89841228b4f5bc4aa6b58b
KyBrMar12 <KyBrMar@gmail.com>
2025-01-09 21:18:53 -0600

    Undoing comment so database gets selected

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,25 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS employeetracker_db;
+
+-- Create the database
+CREATE DATABASE employeetracker_db;
+
+-- Wait for the database to be fully created
+\c employeetracker_db
+
+-- Create the tables
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
+
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL REFERENCES department(id)
 );
+
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
